e5adb13b25af4f4a072e684738ca6877ceba29fc
Opilane TTHK <opilane@tthk.ee>
2020-11-26 11:29:06 +0200

    Create Web Page

---
+++ js/array.js
@@ -0,0 +1,30 @@
+let fruit = ["apple", "banana", "orange"];
+console.log(fruit);
+console.log(fruit[0]);
+console.log(fruit[2]);
+console.log(fruit[fruit.length-1]);
+//FOR Loop
+for(let i = 0; i < 3; i++){
+    let message = `${i+1}.${fruit[i]}`;
+    console.log(message);
+}
+
+
+//FOREACH Loop
+fruit.forEach(element =>  {
+console.log(" Hello from FOREACH ", element);
+});
+
+let rnd = Math.floor(Math.random() * fruit.length);
+console.log(rnd);
+
+let randomFruit = fruit[rnd];
+console.log('My random fruit:', randomFruit);
+
+fruit.push('avocado');
+console.log(fruit);
+
+let bananaIndex = fruit.indexOf('banana');
+console.log('index of banana: ', bananaIndex);
+fruit.splice(bananaIndex, 1);
+console.log(fruit);
